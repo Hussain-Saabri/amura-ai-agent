0549c75fc752881c0df7cd0cf6492082638b5d0b
Hussain-Saabri <dadapir19ce30@gmail.com>
2026-08-19 10:09:06 +0530

    updated

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in environment variables / .env file.")
+    raise ValueError("DATABASE_URL i not set in environment variables / .env file.")
 
 engine = create_engine(DATABASE_URL, pool_size=10, max_overflow=20)
 
